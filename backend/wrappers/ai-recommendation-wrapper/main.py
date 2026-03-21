53d9157697d862a611fbc8d0ddb5a6d86d629f03
Rodney <rwmain07@gmail.com>
2026-03-21 15:41:30 +0800

    Initial

--- backend/wrappers/ai-recommendation-wrapper/main.py
+++ backend/wrappers/ai-recommendation-wrapper/main.py
@@ -1,1 +1,20 @@
-from fastapi import FastAPI, Request, Response, Body
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from groq import Groq
+from prompts import SYSTEM_PROMPT
+
+load_dotenv()
+app = FastAPI()
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+@app.post("/chat")
+async def chat(request: dict):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT },
+            {"role": "user", "content": request["message"]}
+        ],
+        model="llama-3.1-8b-instant"
+    )
+    return {"response": chat_completion.choices[0].message.content}
